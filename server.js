50cfdc7ec896a0d4646cbe70a90f7cc0bd707546
H82M19\Student <Student>
2024-03-04 11:22:53 +0700

    commit debug

--- server.js
+++ server.js
@@ -1,41 +1,66 @@
 const express = require('express');
 const multer = require('multer');
 const app = express();
-const course = require('./Store')
-const upload = multer();
+// const course = require('./Store')
+ AWS_SDK_LOAD_CONFIG=1
 
+// AWS
+const AWS = require("aws-sdk");
+require("dotenv").config();
+// cau hinh AWS
+AWS.config.update({
+    region: process.env.REGION,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKeyId:process.env.SECRET_ACCESS_KEY
+})
+// khoi tao AWS S3  , DynamoDb
+const s3 = new AWS.S3();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = 'Book';
 
+// cau hinh app
 app.use(express.static("./templates"));
 app.set('view engine','ejs');
 app.set('views','./templates');
 
+// cau hinh multer
+const storage = multer.memoryStorage({
+    destination(req,file,callback) {
+        callback(null,'');
+    }
+})
 
-app.get('/',(req,res)=>{
+const upload = multer({
+    storage,
+    limits:{fileSize:2000000}, // gioi han dung luong file
+    fileFilter(req,file,cb) {
+        checkFile(file,cb)
+    }
+})
 
-    // const course = [
-    //     {
-    //         id:1,
-    //         name:'SQL',
-    //         course_type:'CSDL',
-    //         semester:'HK1-2020-2021',
-    //         department:'K.CNTT'
-    //     },
-    //     {
-    //         id:2,
-    //         name:'MySQL',
-    //         course_type:'CSDL',
-    //         semester:'HK1-2020-2021',
-    //         department:'K.CNTT'
-    //     },
-    //     {
-    //         id:3,
-    //         name:'NoSQL',
-    //         course_type:'CSDL',
-    //         semester:'HK1-2020-2021',
-    //         department:'K.CNTT'
-    //     }
-    // ]
-    return res.render("index",{course});
+// Check file
+const checkFile = function(file,cb){
+    const fileType = /jpeg|jpg|png|gif/;
+    const extname = fileType.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileType.test(file.mimetype);
+    if(ext && mimetype) {
+        return cb(null,true);
+    }
+    return cb("Error : Image Only Pls!");
+}
+
+
+app.get('/',async (req,res)=>{
+
+    try {
+        const params = {TableName : tableName};
+        const data = await dynamodb.scan(params).promise();
+        return res.render("index",{data : data.Items});
+    } catch (error) {
+        console.log("Error retrieving data from DynamoDb",error);
+        // return res.status(500).send("Internal Server Error")
+    }
+    
 });
 
 
